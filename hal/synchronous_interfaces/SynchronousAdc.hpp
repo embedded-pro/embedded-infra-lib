b645c63eea46dcf5a7a51c95acb0cb3faa524a86
Gabriel Santos <gabriel.fra.santos@gmail.com>
2025-03-29 19:33:30 +0000

    fix sync adc interface

--- hal/synchronous_interfaces/SynchronousAdc.hpp
+++ hal/synchronous_interfaces/SynchronousAdc.hpp
@@ -1,12 +1,13 @@
 #ifndef HAL_SYNCHRONOUS_ADC_HPP
 #define HAL_SYNCHRONOUS_ADC_HPP
 
+#include "hal/synchronous_interfaces/SynchronousQuadSpi.hpp"
 #include "infra/util/MemoryRange.hpp"
 #include <cstdint>
 
 namespace hal
 {
-    class AdcMultiChannel
+    class SynchronousAdc
     {
     public:
         using Samples = infra::MemoryRange<const uint16_t>;
