8a70bd61dfd129cb633c208273c6cbb371994859
Gabriel Santos <gabriel.fra.santos@gmail.com>
2025-03-30 20:58:05 +0200

    chore: extend pwm interface (#23)
    * add pwm for multiple channels
    
    * extend pwm interface

--- hal/synchronous_interfaces/SynchronousPwm.hpp
+++ hal/synchronous_interfaces/SynchronousPwm.hpp
@@ -12,8 +12,35 @@ namespace hal
     {
     public:
         virtual void SetBaseFrequency(Hertz baseFrequency) = 0;
-        virtual void Start(Percent globalDutyCycle) = 0;
         virtual void Stop() = 0;
+    };
+
+    class SynchronousSingleChannelPwm
+        : public SynchronousPwm
+    {
+    public:
+        virtual void Start(Percent globalDutyCycle) = 0;
+    };
+
+    class SynchronousTwoChannelsPwm
+        : public SynchronousPwm
+    {
+    public:
+        virtual void Start(Percent dutyCycle1, Percent dutyCycle2) = 0;
+    };
+
+    class SynchronousThreeChannelsPwm
+        : public SynchronousPwm
+    {
+    public:
+        virtual void Start(Percent dutyCycle1, Percent dutyCycle2, Percent dutyCycle3) = 0;
+    };
+
+    class SynchronousFourChannelsPwm
+        : public SynchronousPwm
+    {
+    public:
+        virtual void Start(Percent dutyCycle1, Percent dutyCycle2, Percent dutyCycle3, Percent dutyCycle4) = 0;
     };
 }
 
