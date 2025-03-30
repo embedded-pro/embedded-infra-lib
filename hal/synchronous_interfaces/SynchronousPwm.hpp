57ff22c4b3253b51b8729c7c3adcd95df7b71692
Gabriel Santos <gabriel.fra.santos@gmail.com>
2025-03-30 18:56:54 +0000

    extend pwm interface

--- hal/synchronous_interfaces/SynchronousPwm.hpp
+++ hal/synchronous_interfaces/SynchronousPwm.hpp
@@ -8,36 +8,39 @@ namespace hal
     using Hertz = infra::Quantity<infra::Hertz, uint32_t>;
     using Percent = infra::Quantity<infra::Percent, uint8_t>;
 
-    class SynchronousSingleChannelPwm
+    class SynchronousPwm
     {
     public:
         virtual void SetBaseFrequency(Hertz baseFrequency) = 0;
-        virtual void Start(Percent globalDutyCycle) = 0;
         virtual void Stop() = 0;
+    };
+
+    class SynchronousSingleChannelPwm
+        : public SynchronousPwm
+    {
+    public:
+        virtual void Start(Percent globalDutyCycle) = 0;
     };
 
     class SynchronousTwoChannelsPwm
+        : public SynchronousPwm
     {
     public:
-        virtual void SetBaseFrequency(Hertz baseFrequency) = 0;
         virtual void Start(Percent dutyCycle1, Percent dutyCycle2) = 0;
-        virtual void Stop() = 0;
     };
 
     class SynchronousThreeChannelsPwm
+        : public SynchronousPwm
     {
     public:
-        virtual void SetBaseFrequency(Hertz baseFrequency) = 0;
         virtual void Start(Percent dutyCycle1, Percent dutyCycle2, Percent dutyCycle3) = 0;
-        virtual void Stop() = 0;
     };
 
     class SynchronousFourChannelsPwm
+        : public SynchronousPwm
     {
     public:
-        virtual void SetBaseFrequency(Hertz baseFrequency) = 0;
         virtual void Start(Percent dutyCycle1, Percent dutyCycle2, Percent dutyCycle3, Percent dutyCycle4) = 0;
-        virtual void Stop() = 0;
     };
 }
 
