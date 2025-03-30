2683e861a709f61a38a0aff07adbd7b4b3d5a3e5
Gabriel Santos <gabriel.fra.santos@gmail.com>
2025-03-30 18:14:05 +0000

    add pwm for multiple channels

--- hal/synchronous_interfaces/SynchronousPwm.hpp
+++ hal/synchronous_interfaces/SynchronousPwm.hpp
@@ -8,11 +8,35 @@ namespace hal
     using Hertz = infra::Quantity<infra::Hertz, uint32_t>;
     using Percent = infra::Quantity<infra::Percent, uint8_t>;
 
-    class SynchronousPwm
+    class SynchronousSingleChannelPwm
     {
     public:
         virtual void SetBaseFrequency(Hertz baseFrequency) = 0;
         virtual void Start(Percent globalDutyCycle) = 0;
+        virtual void Stop() = 0;
+    };
+
+    class SynchronousTwoChannelsPwm
+    {
+    public:
+        virtual void SetBaseFrequency(Hertz baseFrequency) = 0;
+        virtual void Start(Percent dutyCycle1, Percent dutyCycle2) = 0;
+        virtual void Stop() = 0;
+    };
+
+    class SynchronousThreeChannelsPwm
+    {
+    public:
+        virtual void SetBaseFrequency(Hertz baseFrequency) = 0;
+        virtual void Start(Percent dutyCycle1, Percent dutyCycle2, Percent dutyCycle3) = 0;
+        virtual void Stop() = 0;
+    };
+
+    class SynchronousFourChannelsPwm
+    {
+    public:
+        virtual void SetBaseFrequency(Hertz baseFrequency) = 0;
+        virtual void Start(Percent dutyCycle1, Percent dutyCycle2, Percent dutyCycle3, Percent dutyCycle4) = 0;
         virtual void Stop() = 0;
     };
 }
