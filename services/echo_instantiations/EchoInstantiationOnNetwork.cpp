1292225bafb4efb002db10d65d24be6107437b6d
Gabriel Santos <gabriel.fra.santos@gmail.com>
2026-04-19 20:57:13 +0000

    move echo_instantiations to its own place

--- services/echo_instantiations/EchoInstantiationOnNetwork.cpp
+++ services/echo_instantiations/EchoInstantiationOnNetwork.cpp
@@ -1,4 +1,4 @@
-#include "services/network/echo/EchoInstantiationOnNetwork.hpp"
+#include "services/echo_instantiations/EchoInstantiationOnNetwork.hpp"
 #include "hal/generic/SynchronousRandomDataGeneratorGeneric.hpp"
 #include "infra/timer/Waiting.hpp"
 #include "services/tracer/TracingEchoInstantiation.hpp"
