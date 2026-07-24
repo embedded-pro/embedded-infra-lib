a56b6454ffa40e1ada080beb2c860f0bc576634c
gfs <gabriel.fra.santos@gmail.com>
2026-07-24 17:42:07 +0000

    disable warning as error for test lib

--- cmake/emil_test_helpers.cmake
+++ cmake/emil_test_helpers.cmake
@@ -19,6 +19,7 @@ function(emil_fetch_googletest)
         endif()
     endforeach()
 
+    set_target_properties(gtest gtest_main gmock gmock_main PROPERTIES COMPILE_WARNING_AS_ERROR Off)
     set_target_properties(gtest gtest_main gmock gmock_main PROPERTIES FOLDER External/GoogleTest)
     set_target_properties(gtest gtest_main gmock gmock_main PROPERTIES EXCLUDE_FROM_COVERAGE TRUE)
     mark_as_advanced(BUILD_GMOCK BUILD_GTEST BUILD_SHARED_LIBS gmock_build_tests gtest_build_samples test_build_tests gtest_disable_pthreads gtest_force_shared_crt gtest_hide_internal_symbols)
